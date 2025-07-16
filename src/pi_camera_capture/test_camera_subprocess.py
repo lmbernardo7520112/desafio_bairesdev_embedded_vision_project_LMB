3863876e9a8a9202584315602cada1dda0fb1d0e
lmbernardo7520112 <lmbernardo752011@gmail.com>
2025-07-16 16:22:13 -0300

    feat: add script to run camera test on Pi 3 via subprocess + local Python version tracking

---
+++ src/pi_camera_capture/test_camera_subprocess.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# Envia o comando para rodar o script remotamente na Pi
+result = subprocess.run([
+    "ssh", "lmbernardo@192.168.1.195",
+    "source ~/desafio_bairesdev_embedded_vision_project_LMB/embedded_vision_project_venv/bin/activate && "
+    "python ~/desafio_bairesdev_embedded_vision_project_LMB/src/pi_camera_capture/test_camera.py"
+], capture_output=True, text=True)
+print(result.stdout)
+print(result.stderr)
